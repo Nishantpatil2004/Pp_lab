a99d6245747ce436f665683e56ebeeb4bfc335ec
Nishantpatil2004 <134784964+Nishantpatil2004@users.noreply.github.com>
2023-12-08 11:54:12 +0530

    Update Com_2.cpp

--- Com_2.cpp
+++ Com_2.cpp
@@ -1,1 +1,55 @@
+#include<iostream> using namespace std;
 
+
+
+class BB;
+
+class AA
+{
+
+
+
+private:
+
+int v1;
+public:
+
+void setdata(int val){
+v1=val;
+}
+
+
+int display()
+{
+
+return v1;}
+
+friend void exchange (AA &x, BB &y); };
+
+class BB
+
+{
+
+private:
+
+int v2;
+
+public:
+
+void setdata(int val)
+
+{
+
+v2=val;
+
+}
+
+int display()
+
+{
+
+return v2;
+
+} friend void exchange (AA &x, BB &y);
+};
+void exchange(AA 
